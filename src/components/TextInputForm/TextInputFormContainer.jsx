941950f347f185ee1bb9e00f1dd61b84827a8e07
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2025-03-18 23:04:40 +0530

    Added form submit button

--- src/components/TextInputForm/TextInputFormContainer.jsx
+++ src/components/TextInputForm/TextInputFormContainer.jsx
@@ -5,14 +5,16 @@ function TextInputFormContainer(){
    
 
   let [inputType , setInputType] = useState("password");
+  let [value , setValue] = useState("")
   function handleFormSubmit(event){
     event.preventDefault();             {/* used to prevent default property of form tag i.e, refresh the page */}
-    console.log("Form submitted")
+    console.log("Form submitted" , value)
   }
 
   function handleTextInputChange(event){
      console.log("text input change");
-     console.log(event.target.value)
+     console.log(event.target.value);
+     setValue(event.target.value);
   }
 
   function handleShowHideClick(){
