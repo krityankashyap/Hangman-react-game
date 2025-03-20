78f9057a572a5c467c407a2bbe7f8e1014401371
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2025-03-20 19:29:07 +0530

    used useLocation() react hook

--- src/pages/PlayGame.jsx
+++ src/pages/PlayGame.jsx
@@ -1,14 +1,14 @@
-import { Link, useSearchParams } from "react-router-dom"
+import { Link, useLocation,  } from "react-router-dom"
 
 // const [ searchParams ] = useSearchParams();
 // console.log(searchParams.get("text"));
 
 function PlayGame(){
 
- 
+const { state } = useLocation();
  return (
   <>
-   <h>Play Game</h>
+   <h>Play Game {state.wordSelected}</h>
    <Link to="/start" className="text-green-500 px-4 py-2">Start-Game</Link>
   
   </>
