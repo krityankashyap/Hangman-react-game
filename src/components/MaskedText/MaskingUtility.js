d5de7036ce002720a217f63669e96610d245a7f7
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2025-03-21 17:23:34 +0530

    Added buttons logic and done some correctness to prev codes in play-page

--- src/components/MaskedText/MaskingUtility.js
+++ src/components/MaskedText/MaskingUtility.js
@@ -4,6 +4,7 @@
  * @param {Letters which are guessed by the user already} guessedLetters 
  */
 export function getMaskedString(originalWord , guessedLetters){
+  // console.log('guessed letters' , guessedLetters)
   guessedLetters = guessedLetters.map(letter => letter.toUpperCase()); // convert all guessed letters in upper case 
  
   const guessedLetterSet = new Set(guessedLetters);
--- src/pages/PlayGame.jsx
+++ src/pages/PlayGame.jsx
@@ -1,19 +1,29 @@
 import { Link, useLocation,  } from "react-router-dom"
 import Maskedtext from "../components/MaskedText/MaskedText";
 import LetterButtons from "../components/LetterButtons/LetterButtons";
-
-// const [ searchParams ] = useSearchParams();
-// console.log(searchParams.get("text"));
+import { useState } from "react";
+ 
 
 function PlayGame(){
 
-const { state } = useLocation();
+  
+  const [guessedLetters , setGuessedLetter] = useState([]);
+
+  const { state } = useLocation();
+
+  function handleLetterClick(letter){
+    setGuessedLetter([...guessedLetters , letter])
+  }
+
+
  return (
   <>
    <h>Play Game</h>
-   <Maskedtext text={state.wordSelected} guessedLetters={[]} />
-   
-   <div>  <LetterButtons text={state.wordSelected} guessedLetters={[]} onLetterClick={()=>{}} />  </div>
+   <Maskedtext text={state.wordSelected} guessedLetters={guessedLetters} />
+  <div>
+  <LetterButtons text={state.wordSelected} guessedLetters={guessedLetters} onLetterClick={handleLetterClick} /> 
+  </div>
+    
    
    <Link to="/start" className="text-green-500 px-4 py-2">Start-Game</Link>
   
